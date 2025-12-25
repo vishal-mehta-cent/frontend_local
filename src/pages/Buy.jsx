ff2c2b8ec6a532e3e4bdc0cc8cbb4a99c752258a
vishal-mehta-cent <vishal.centpercentanalytics@gmail.com>
2025-12-25 15:41:30 +0530

    Update Buy.jsx

--- src/pages/Buy.jsx
+++ src/pages/Buy.jsx
@@ -8,7 +8,7 @@ function isMarketOpenUTC() {
   const nowUTC = new Date();
   const minutes = nowUTC.getUTCHours() * 60 + nowUTC.getUTCMinutes();
   const OPEN = 3 * 60 + 45;
-  const CLOSE = 11 * 30 + 0;
+  const CLOSE = 11 * 60 + 30;
   return minutes >= OPEN && minutes <= CLOSE;
 }
 
