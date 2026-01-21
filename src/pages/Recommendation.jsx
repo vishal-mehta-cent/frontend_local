02e7b0e8505e88b85d1413435dfdb5128723ef6e
vishal-mehta-cent <vishal.centpercentanalytics@gmail.com>
2026-01-21 15:41:16 +0530

    Update Recommendation.jsx

--- src/pages/Recommendation.jsx
+++ src/pages/Recommendation.jsx
@@ -983,7 +983,7 @@ export default function Recommendations() {
 
 
       {/* ===== MAIN CONTENT (STEP 5) ===== */}
-      <div className="w-full px-3 sm:px-4 md:px-6 py-6 relative pb-24 pt-[140px] sm:pt-[195px]">
+      <div className="w-full px-3 sm:px-4 md:px-6 py-6 relative pb-24">
 
         <div className="mb-6">
           <h2 className={`text-4xl font-bold ${textClass} mb-2`}>
