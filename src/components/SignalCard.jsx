89a00b89ec61e5be29a261191e2af30d12e4dcd0
vishal-mehta-cent <vishal.centpercentanalytics@gmail.com>
2026-02-09 11:26:52 +0530

    Update SignalCard.jsx

--- src/components/SignalCard.jsx
+++ src/components/SignalCard.jsx
@@ -154,7 +154,7 @@ export default function SignalCard({
     if (outHour > 12) outHour -= 12;
     if (outHour === 0) outHour = 12;
 
-    return `${outDay}/${outMonth} | ${outHour}:${outMinutes} ${outAMPM}`;
+    return `${outMonth}/${outDay} | ${outHour}:${outMinutes} ${outAMPM}`;
   };
 
   const formattedCloseDT = formatCloseDateTime(closeTime);
